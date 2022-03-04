df81124123c8a2d5ec6d99b770f09f11818f93bf
Adam Buczek <abadambuczek@gmail.com>
2022-03-04 23:02:51 +0100

    working

--- src/Form/LogType.php
+++ src/Form/LogType.php
@@ -2,7 +2,7 @@
 
 namespace App\Form;
 
-use App\Entity\Notice;
+use App\Entity\Log;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
