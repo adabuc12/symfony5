3396b41234a05a2fc51d7142540c349d32bfb3b5
Adam Buczek <abadambuczek@gmail.com>
2022-08-12 21:52:21 +0200

    working

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -157,7 +157,7 @@ return $this->renderForm('order/new.html.twig', [
      */
     public function export(Order $order): Response
     {
-    $url = "91.218.202.254:5000/api/table/?tablename=firma";
+    $url = "127.0.0.1:5000/api/table/?tablename=firma";
     $ch = curl_init();
     curl_setopt($ch,CURLOPT_URL,$url);
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
