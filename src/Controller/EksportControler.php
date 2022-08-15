580248e11713b9807573c2a0644999bb02a532c4
Adam Buczek <abadambuczek@gmail.com>
2022-08-15 20:08:23 +0200

    new work

--- src/Controller/EksportControler.php
+++ src/Controller/EksportControler.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\DbLog;
 use App\Entity\Order;
+use App\Repository\ProductRepository;
 use DateTime;
 
 /**
@@ -32,8 +33,46 @@ class EksportControler extends AbstractController {
             echo curl_error($ch);
         }
         curl_close($ch);
-        print_r(json_decode($json));
-        exit;
+        $result = json_decode($json);
+        var_dump($result);
+        
+        return $this->redirectToRoute('cart');
+    }
+    
+     /**
+     * @Route("/syncproducts/{type}", name="products_sync")
+     */
+    public function productsSync(string $type, DbLog $logger, ProductRepository $productRepository): Response {
+
+        //get products from wapro
+        $url = "127.0.0.1:5000/api/Table/?tablename=artykul";
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch,CURLOPT_CONNECTTIMEOUT, 4);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('api_key: 039100D6-D42B-4A06-875D-4E5466B12D45'));
+        $json = curl_exec($ch);
+        if(!$json) {
+            echo curl_error($ch);
+        }
+        curl_close($ch);
+        $products_wapro = json_decode($json);
+        var_dump($products_wapro);
+        exit();
+        //get products from kolodomu panel
+        $products_kolo = $productRepository->findAllPriceGreaterThanZero();
+        var_dump($products_kolo);
+        exit();
+        //check if products is in specific type database
+        if($type == 'wapro'){
+            
+        }
+        if($type == 'kolo_panel'){
+            
+        }
+        //print result of synchronisation
+        
+        
         
         return $this->redirectToRoute('cart');
     }
